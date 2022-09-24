6b2f14fa959b62fdc8ce54d6e0fb247073acbd51
opee2012 <jborth2010@gmail.com>
2022-09-24 18:03:05 -0400

    Added documentation

--- sets.js
+++ sets.js
@@ -1,3 +1,19 @@
+/*
+    CSCI 390, Set Operations, Jacob Borth, 24 September 2022
+
+    -u SET1 SET2, --union SET1 SET2
+    Outputs a set in Array form of elements in Number form that are either in SET1 or SET2
+
+    -i SET1 SET2, --intersection SET1 SET2
+    Outputs a set in Array form of elements in Number form that are in *both* SET1 and SET2
+
+    -d SET1 SET2, --difference SET1 SET2
+    Outputs a set in Array form of elements in Number form that are in SET1 unless they are in SET2
+
+    -h, --help
+    Output a usage message and exit.
+*/
+
 // Union - Duplicate first set then compare that with second set. If there are duplicates, don't add to array.
 function setUnion() {
     var unionSet = new Array();
